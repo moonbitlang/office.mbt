ac3bb0c3b24b51e36c6b8c198f74798dde3faa15
Hongbo Zhang <bobzhang1988@gmail.com>
2026-02-09 20:34:37 +0800

    test: add matrix-smoke contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract-contract checker contract guard (part 410)

--- scripts/check_parity_wrappers.sh
+++ scripts/check_parity_wrappers.sh
@@ -55,6 +55,7 @@ wrappers=(
   "scripts/check_parity_preflight_matrix_smoke_contract_x25.sh"
   "scripts/check_parity_preflight_matrix_smoke_contract_x26.sh"
   "scripts/check_parity_preflight_matrix_smoke_contract_x27.sh"
+  "scripts/check_parity_preflight_matrix_smoke_contract_x28.sh"
   "scripts/show_parity_env.sh"
 )
 
