17aa6604af60192af85a46eeeea03d5fe7826dbe
Hongbo Zhang <bobzhang1988@gmail.com>
2026-02-09 22:03:22 +0800

    test: add matrix-smoke contract checker x51 (part 456)

--- scripts/check_parity_wrappers.sh
+++ scripts/check_parity_wrappers.sh
@@ -78,6 +78,7 @@ wrappers=(
   "scripts/check_parity_preflight_matrix_smoke_contract_x48.sh"
   "scripts/check_parity_preflight_matrix_smoke_contract_x49.sh"
   "scripts/check_parity_preflight_matrix_smoke_contract_x50.sh"
+  "scripts/check_parity_preflight_matrix_smoke_contract_x51.sh"
   "scripts/show_parity_env.sh"
 )
 
